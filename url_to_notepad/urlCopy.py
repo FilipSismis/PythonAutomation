c32bf2687ac633124da969097eb3a36a17027dcb
FilipSismis <78262016+FilipSismis@users.noreply.github.com>
2022-09-22 01:44:25 +0200

    better functionality
    added an option to name .txt file and added reading of any .txt file within the directory of script

--- url_to_notepad/urlCopy.py
+++ url_to_notepad/urlCopy.py
@@ -1,5 +1,6 @@
 import keyboard as keyboard
 import time
+import os
 import win32clipboard
 
 print('(1)...From browser to notepad' + '\n' +
@@ -7,8 +8,10 @@ print('(1)...From browser to notepad' + '\n' +
     '(3)...Exit' + '\n' + 
     '-----------------------------')
 choice = input("Enter your choice:")
+
 if choice == '1':
     print('(1)...From browser to notepad')
+    nameOfFile = input("Enter name of the .txt file (you can leave blank as well):")
     numberOfTabs = int(input("Enter number of tabs:"))
     links =[]
     
@@ -16,17 +19,22 @@ if choice == '1':
     time.sleep(2)
     
     for i in range(numberOfTabs):
-        keyboard.press_and_release('ctrl+tab')
-        time.sleep(0.1)
         keyboard.press_and_release('ctrl+l')
         time.sleep(0.1)
         keyboard.press_and_release('ctrl+c')
         time.sleep(0.1)
+        keyboard.press_and_release('ctrl+tab')
+        time.sleep(0.1)
         win32clipboard.OpenClipboard()
         links.append(win32clipboard.GetClipboardData())
         win32clipboard.CloseClipboard()
-    
-    file = open("New Text Document.txt" , "a")
+
+    if not nameOfFile:
+        nameOfFile = "New Text Document.txt"
+    if not nameOfFile.endswith(".txt"):
+        nameOfFile = nameOfFile + ".txt"
+        
+    file = open(nameOfFile , "a")
     for link in links:
         file.write(link + "\n")
     file.close
@@ -35,10 +43,19 @@ elif choice == '2':
     print('(2)...From notepad to browser')
     print('Sleeping for 2 seconds')
     time.sleep(2)
+
+    filePath = ""
+    fileList = os.listdir('.//')
     
-    file = open("New Text Document.txt" , "r")
+    for file in fileList:
+        if(file.endswith(".txt")):
+            filePath = file
+            continue    
+            
+    file = open(filePath , "r")
     links = file.read().splitlines()
     file.close
+    
     for link in links:
         win32clipboard.OpenClipboard()
         win32clipboard.EmptyClipboard()
